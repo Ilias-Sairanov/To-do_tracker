8b19a71452033266f701df7933e453ca02155767
Ilias Sairanov <ilyas.sayranov@inbox.ru>
2021-04-03 17:29:28 +0700

    init

---
+++ To-do_tracker.Tests/UnitTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace To_do_tracker.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ To-do_tracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace To_do_tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
